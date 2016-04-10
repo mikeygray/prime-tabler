7765facecdebf665b0578ba3745acfa4cb83f024
Mikey Gray <michael.r.a.gray@gmail.com>
2016-04-10 22:42:15 +0100

    Prime Generation and Primes List Object
    Added a Test Project and what I'm calling the "PrimesModel". This will
    be the object responsible for calculating the prime list and storing it
    for use in a UI.

---
+++ PrimeTablerModel/PrimesModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeTabler.PrimeTablerModel
+{
+
+    /// <summary>
+    /// An object for the calculation and storing of prime numbers and associated multiplcation table
+    /// </summary>
+    public class PrimesModel
+    {
+        public List<long> Primes
+        {
+            get { return _primes; }
+        }
+
+        public TimeSpan CalculationTime
+        {
+            get { return _calculationTime; }
+        }
+
+        public int NumberOfPrimes
+        {
+            get { return _primes.Count; }
+        }
+
+        private List<long> _primes = new List<long>();
+        private TimeSpan _calculationTime = new TimeSpan();
+
+        public PrimesModel(int numberOfPrimes)
+        {
+            PopulatePrimes(numberOfPrimes);
+        }
+
+        /// <summary>
+        /// Builds this objects list of sequential Prime Numbers starting with 2
+        /// </summary>
+        /// <param name="numberOfPrimes">i.e. 3 would populate {2,3,5}</param>
+        public void PopulatePrimes(int numberOfPrimes)
+        {
+            _primes.Clear();
+            var startTime = DateTime.Now;
+            if (numberOfPrimes > 0) _primes.Add(2);
+            long nextPrime = 3;
+
+            while (Primes.Count < numberOfPrimes)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; _primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % _primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    _primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+            _calculationTime = (DateTime.Now).Subtract(startTime);
+        }
+    }
+}
---
+++ PrimeTablerTest/PrimeModelTest.cs
@@ -0,0 +1,115 @@
+﻿using PrimeTabler.PrimeTablerModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PrimeTabler.PrimeTablerTest
+{
+    [TestClass]
+    public class PrimeModelTest
+    {
+        /* Negative Input */
+        [TestMethod]
+        public void WhenConstructorNegative_ShouldPopulateNoPrimes()
+        {
+            var primes = new PrimesModel(-1);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+
+            primes = new PrimesModel(-500);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenPopulatePrimesNegative_ShouldPopulateNoPrimes()
+        {
+            var primes = new PrimesModel(3);
+            primes.PopulatePrimes(-1);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+
+            primes = new PrimesModel(7);
+            primes.PopulatePrimes(-239);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+        }
+
+        /* Zero Input */
+        [TestMethod]
+        public void WhenConstructorZero_ShouldPopulateNoPrimes()
+        {
+            var primes = new PrimesModel(0);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenPopulatePrimesZero_ShouldPopulateNoPrimes()
+        {
+            var primes = new PrimesModel(4);
+            primes.PopulatePrimes(0);
+            CollectionAssert.AreEqual(new List<long> { }, primes.Primes);
+            Assert.AreEqual(0, primes.NumberOfPrimes);
+        }
+
+        /* Positive Input */
+        [TestMethod]
+        public void WhenConstructor1Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(1);
+            CollectionAssert.AreEqual(new List<long> { 2 }, primes.Primes);
+            Assert.AreEqual(1, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenPopulatePrimes1Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(13);
+            primes.PopulatePrimes(1);
+            CollectionAssert.AreEqual(new List<long> { 2 }, primes.Primes);
+            Assert.AreEqual(1, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenConstructor3Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(3);
+            CollectionAssert.AreEqual(new List<long> { 2, 3, 5 }, primes.Primes);
+            Assert.AreEqual(3, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenPopulatePrimes3Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(0);
+            primes.PopulatePrimes(3);
+            CollectionAssert.AreEqual(new List<long> { 2, 3, 5 }, primes.Primes);
+            Assert.AreEqual(3, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenConstructor10Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(10);
+            CollectionAssert.AreEqual(new List<long> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 }, primes.Primes);
+            Assert.AreEqual(10, primes.NumberOfPrimes);
+        }
+
+        [TestMethod]
+        public void WhenPopulatePrimes10Prime_ShouldPopulateAppropiately()
+        {
+            var primes = new PrimesModel(-5);
+            primes.PopulatePrimes(10);
+            CollectionAssert.AreEqual(new List<long> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 }, primes.Primes);
+            Assert.AreEqual(10, primes.NumberOfPrimes);
+        }
+
+        /* Calculation Time */
+        [TestMethod]
+        public void WhenCalculating1000Primes_ShouldHaveCalculationTimeOverZero()
+        {
+            var primes = new PrimesModel(1000);
+            Assert.IsTrue(primes.CalculationTime.TotalSeconds > 0);
+        }
+    }
+}
